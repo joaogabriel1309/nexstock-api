b328313a36d1e0c675afe54854d699725010bdbc
joao.gabriel <gabriel.122.gm85@gmail.com>
2026-03-04 13:25:13 -0400

    chore: inicializa projeto com arquitetura em camadas.

---
+++ src/main/java/br/com/nexstock/nexstock_api/NexstockApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.nexstock.nexstock_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexstockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexstockApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nexstock/nexstock_api/NexstockApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nexstock.nexstock_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexstockApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
