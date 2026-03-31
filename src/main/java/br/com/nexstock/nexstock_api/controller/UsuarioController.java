2979eb982b6031ddf14634383029a3217bca6945
João Gabriel de Matos Silva <74660748+joaogabriel1309@users.noreply.github.com>
2026-03-31 18:17:05 -0400

    feat(usuario): implementar o controller do usuario, implementa o end-point de criação, listar todos por empresa, buscar por id e deletar.

---
+++ src/main/java/br/com/nexstock/nexstock_api/controller/UsuarioController.java
@@ -0,0 +1,46 @@
+package br.com.nexstock.nexstock_api.controller;
+
+import br.com.nexstock.nexstock_api.dto.request.RegistroUsuarioRequest;
+import br.com.nexstock.nexstock_api.dto.request.UsuarioRequest;
+import br.com.nexstock.nexstock_api.dto.response.UsuarioResponse;
+import br.com.nexstock.nexstock_api.service.UsuarioService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
+public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioResponse> cria(@RequestBody @Valid RegistroUsuarioRequest request) {
+        UsuarioResponse response = usuarioService.criar(request);
+        return ResponseEntity.status(org.springframework.http.HttpStatus.CREATED).body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioResponse>> listarTodoPorEmpresa(@RequestParam UUID empresaId) {
+        return ResponseEntity.ok(usuarioService.listarTodoPorEmpresa(empresaId));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UsuarioResponse> buscarPorId(@RequestParam UUID empresaId, @PathVariable UUID id) {
+        return ResponseEntity.ok(usuarioService.buscarPorId(id, empresaId));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(
+            @RequestParam UUID empresaId,
+            @PathVariable UUID id) {
+
+        usuarioService.deletarUsuario(id, empresaId);
+        return ResponseEntity.noContent().build();
+    }
+}
